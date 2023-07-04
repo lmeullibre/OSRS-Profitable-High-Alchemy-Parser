34e52404ddedb98b6bbf230d01eb2bc3614cae6b
Sergi Martínez <sergimartinezrodriguez@gmail.com>
2023-07-04 23:01:28 +0200

    changed behavior since original was trash

--- main.py
+++ main.py
@@ -5,58 +5,34 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
-def scrape_website():
+def scrape_data():
     url = "https://oldschool.runescape.wiki/w/RuneScape:Grand_Exchange_Market_Watch/Alchemy"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     table = soup.select_one('.wikitable.sortable.sticky-header')
 
-    # Get the header
-    header = [col.get_text(strip=True) for col in table.find_all('th')]
-
-    # Find all rows in the table
-    rows = table.find_all('tr')[1:]  # Skip the header row
-
-    # This will store the row data
     data = []
-
-    # Loop over the rows
-    for i, row in enumerate(rows):
-        # Find all columns in the row, excluding the first (icon) column
-        cols = row.find_all('td')[1:]
+    rows = table.select('tbody > tr')
+    for row in rows:
+        cols = row.select('td')
         
-        # Get the text inside each <td>
-        cols = [col.get_text(strip=True) if col.get_text(strip=True) else 'N/A' for col in cols]
+        # Ensure the row has enough columns
+        if len(cols) >= 10:
+            item_data = {
+                "Item": cols[1].get_text(strip=True),
+                "GE Price": int(cols[2].get_text(strip=True).replace(',', '')),
+                "High Alch": int(cols[3].get_text(strip=True).replace(',', '')),
+                "Profit": int(cols[4].get_text(strip=True).replace(',', '')),
+                "ROI%": round(float(cols[5].get_text(strip=True).replace('%', ''))/100, 4),
+                "Limit": int(cols[6].get_text(strip=True).replace(',', '')),
+                "Volume": int(cols[7].get_text(strip=True).replace(',', '')),
+                "Max profit": int(cols[8].get_text(strip=True).replace(',', '')),
+                "Members": cols[9]['data-sort-value'].lower() == 'true',  # Convert string to boolean
+            }
+            data.append(item_data)
 
-        # If there are not enough columns, fill in the missing ones with 'N/A'
-        if len(cols) < len(header):
-            cols.extend(['N/A'] * (len(header) - len(cols)))
-
-        # Check the "members" column and replace its value based on the "data-sort-value" attribute
-        for j, col in enumerate(row.find_all('td')[1:]):
-            if header[j] == "Members":
-                cols[j] = True if col['data-sort-value'] == 'true' else False
-            elif header[j] == "ROI%":
-                if cols[j] != 'N/A':
-                    # Convert the string to a float, divide it by 100, and round to 3 decimal places
-                    cols[j] = round(float(cols[j].replace('%', '')) / 100, 3)
-            else:
-                # Convert the other columns to integer if possible
-                if cols[j] != 'N/A':
-                    try:
-                        cols[j] = int(cols[j].replace(',', ''))
-                    except ValueError:
-                        cols[j] = None  # Or set another appropriate default value
-
-        # Create a dictionary for this row
-        row_dict = dict(zip(header, cols))
-
-        # Add the row's dictionary to our data array
-        data.append(row_dict)
-
-    return jsonify({"data": data})
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
